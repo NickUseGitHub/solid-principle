add56270ba388572fbea34519bb014effef78573
Chaiwtun <chaiwtun@scg.com>
2021-05-06 00:04:54 +0700

    [master] refactor code

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,10 @@ export default function run() {
   const tigerMove = new AnimalMove({ speed: 20 });
   const lambogini = new VehicleMove({ speed: 240 });
 
+  console.log('----------- check movable object -----------');
   console.log('tigerMove MoveDirection.NORTH', tigerMove.move(MoveDirection.NORTH));
   console.log('tigerMove MoveDirection.SOUTH', tigerMove.move(MoveDirection.SOUTH));
   console.log('lambogini MoveDirection.NORTH', lambogini.move(MoveDirection.NORTH));
   console.log('lambogini MoveDirection.SOUTH', lambogini.move(MoveDirection.SOUTH));
+  console.log('-------------------------------------------------------');
 }
